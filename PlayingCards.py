c03b1100110adb066e4a2936493d9ec4bae87ae6
scarf <youkim005@knou.ac.kr>
2021-10-13 20:44:48 +0900

    refactor: :recycle: extracted function append_symbol_set_to_cards() to apply to all four symbols

--- PlayingCards.py
+++ PlayingCards.py
@@ -1,22 +1,22 @@
 cards = [""]
 
-# card[1 ~ 13] 스페이드 카드
 values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 
-for i in range(len(values)):
-    cards.append("Spade " + str(values[i]))
 
+def append_symbol_set_to_cards(symbol: str):
+    for i in range(len(values)):
+        cards.append(symbol + " " + str(values[i]))
+
+
+# card[1 ~ 13] 스페이드 카드
+append_symbol_set_to_cards("Spade")
 # card[14 ~ 26] 다이아몬드 카드
-for i in range(len(values)):
-    cards.append("Diamond " + str(values[i]))
-
+append_symbol_set_to_cards("Diamond")
 # card[27 ~ 39] 하트 카드
-for i in range(len(values)):
-    cards.append("Heart " + str(values[i]))
-
+append_symbol_set_to_cards("Heart")
 # card[40 ~ 52] 하트 카드
-for i in range(len(values)):
-    cards.append("Club " + str(values[i]))
+append_symbol_set_to_cards("Club")
+
 
 if __name__ == "__main__":
     from pprint import pprint
