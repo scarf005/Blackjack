6fcfb18883980d1c4b13f1eb884b1ecb0fac0419
youkim <83401142+youkim005@users.noreply.github.com>
2021-10-14 13:52:25 +0900

    chore: :truck: rename playingcards.py
    also removed unnecessary docs

--- playingcards.py
+++ playingcards.py
@@ -1,9 +1,3 @@
-"""
-1 ~ 13: 스페이드
-14 ~ 26: 다이아몬드
-27 ~ 39: 하트
-40 ~ 52: 클럽
-"""
 Cards = list[str]
 values: list[str | int] = ["A", *range(2, 10 + 1), "J", "Q", "K"]
 
@@ -14,9 +8,3 @@ def create_symbol_set(symbol: str):
 cards: Cards = []
 for symbol in ["Spade", "Diamond", "Heart", "Club"]:
     cards.extend(create_symbol_set(symbol))
-
-
-if __name__ == "__main__":
-    from pprint import pprint
-
-    pprint(cards)
